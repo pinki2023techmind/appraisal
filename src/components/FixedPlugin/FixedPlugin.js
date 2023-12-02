f4b6796ac56b21b3071fc9794826191b2f51dd4d
pinki <pinki@techmindscape.in>
2023-12-02 17:25:34 +0530

    first

---
+++ src/components/FixedPlugin/FixedPlugin.js
@@ -0,0 +1,41 @@
+
+import React, { Component } from "react";
+
+import { Dropdown, Badge, Button, Form } from "react-bootstrap";
+
+import sideBarImage1 from "assets/img/sidebar-1.jpg";
+import sideBarImage2 from "assets/img/sidebar-2.jpg";
+import sideBarImage3 from "assets/img/sidebar-3.jpg";
+import sideBarImage4 from "assets/img/sidebar-4.jpg";
+
+function FixedPlugin({
+  hasImage,
+  setHasImage,
+  color,
+  setColor,
+  image,
+  setImage
+}) {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     classes: "dropdown show-dropdown open",
+  //     bg_checked: true,
+  //     bgImage: this.props.bgImage,
+  //   };
+  // }
+  // handleClick = () => {
+  //   this.props.handleFixedClick();
+  // };
+  // onChangeClick = () => {
+  //   this.props.handleHasImage(!this.state.bg_checked);
+  //   this.setState({ bg_checked: !this.state.bg_checked });
+  // };
+  return (
+    <div className="fixed-plugin">
+     
+    </div>
+  );
+}
+
+export default FixedPlugin;
